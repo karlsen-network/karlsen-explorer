32a7a56a3ee6afee1d170864bcdb42a3a6b38079
Leon1777 <168653957+Leon1777@users.noreply.github.com>
2024-10-24 14:47:33 +0200

    remove block/headerCount and refactor hashrate unit assignment

--- src/components/BlockDAG.js
+++ src/components/BlockDAG.js
@@ -5,18 +5,25 @@ import { getBlockdagInfo, getKarlsendInfo } from "../karlsen-api-client";
 
 const BlockDAGBox = () => {
   const [networkName, setNetworkName] = useState("");
-  const [blockCount, setBlockCount] = useState();
-  const [headerCount, setHeaderCount] = useState("");
   const [virtualDaaScore, setVirtualDaaScore] = useState("");
   const [hashrate, setHashrate] = useState("");
   const [mempool, setMempool] = useState("");
 
   // calculate and assign correct hashrate unit
-  const calculateAndFormatHashrate = (difficulty) => {
-    const hashrateValue = ((difficulty * 2) / 1000000000000).toFixed(2);
-    return parseFloat(hashrateValue) >= 1
-      ? `${hashrateValue} TH/s`
-      : `${(parseFloat(hashrateValue) * 1000).toFixed(2)} GH/s`;
+  const formatHashrate = (hashraw, decimals) => {
+    let hashrateValue = hashraw;
+    let unit = "H/s";
+    if (hashrateValue >= 1e12) {
+      hashrateValue = hashrateValue / 1e12;
+      unit = "TH/s";
+    } else if (hashrateValue >= 1e9) {
+      hashrateValue = hashrateValue / 1e9;
+      unit = "GH/s";
+    } else if (hashrateValue >= 1e6) {
+      hashrateValue = hashrateValue / 1e6;
+      unit = "MH/s";
+    }
+    return `${hashrateValue.toFixed(decimals)} ${unit}`;
   };
 
   // wrap initBox in useCallback to avoid dependency of useEffect
@@ -27,10 +34,9 @@ const BlockDAGBox = () => {
     console.log("DAG Info ", dag_info);
 
     setNetworkName(dag_info.networkName);
-    setBlockCount(dag_info.blockCount);
-    setHeaderCount(dag_info.headerCount);
     setVirtualDaaScore(dag_info.virtualDaaScore);
-    setHashrate(calculateAndFormatHashrate(dag_info.difficulty));
+    const hashraw = dag_info.difficulty * 2;
+    setHashrate(formatHashrate(hashraw, 2));
     setMempool(karlsendInfo.mempoolSize);
   }, []);
 
@@ -39,29 +45,12 @@ const BlockDAGBox = () => {
     const updateInterval = setInterval(async () => {
       const dag_info = await getBlockdagInfo();
       setNetworkName(dag_info.networkName);
-      setBlockCount(dag_info.blockCount);
-      setHeaderCount(dag_info.headerCount);
       setVirtualDaaScore(dag_info.virtualDaaScore);
-      setHashrate(calculateAndFormatHashrate(dag_info.difficulty));
+      const hashraw = dag_info.difficulty * 2;
+      setHashrate(formatHashrate(hashraw, 2));
     }, 60000);
     return () => clearInterval(updateInterval);
   }, [initBox]);
-
-  useEffect(() => {
-    document
-      .getElementById("blockCount")
-      .animate([{ opacity: "1" }, { opacity: "0.6" }, { opacity: "1" }], {
-        duration: 300,
-      });
-  }, [blockCount]);
-
-  useEffect(() => {
-    document
-      .getElementById("headerCount")
-      .animate([{ opacity: "1" }, { opacity: "0.6" }, { opacity: "1" }], {
-        duration: 300,
-      });
-  }, [headerCount]);
 
   useEffect(() => {
     document
@@ -101,18 +90,6 @@ const BlockDAGBox = () => {
           <tr>
             <td className="cardBoxElement">Network name</td>
             <td className="pt-1 text-nowrap">{networkName}</td>
-          </tr>
-          <tr>
-            <td className="cardBoxElement">Block count</td>
-            <td className="pt-1" id="blockCount">
-              {blockCount}
-            </td>
-          </tr>
-          <tr>
-            <td className="cardBoxElement">Header count</td>
-            <td className="pt-1" id="headerCount">
-              {headerCount}
-            </td>
           </tr>
           <tr>
             <td className="cardBoxElement">Mempool count</td>
